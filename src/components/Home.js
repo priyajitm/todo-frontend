018273f95b74d2acf7fb1fc003e2ea9423dd0959
Priyajit Mukherjee <priyajit.mukherjee1@gmail.com>
2022-11-13 14:58:46 +0530

    added dashboard link

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
              <h1>My ToDo List App</h1>
              <Link to="/login"><button>Login</button></Link>
              <Link to="/signup"><button>Signup</button></Link>
+             <Link to="/dashboard"><button>Dashboard</button></Link>
         </div>
        
     )
